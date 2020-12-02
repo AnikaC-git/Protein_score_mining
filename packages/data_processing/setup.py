ed501bc453ed4058fe5419e58904e99e747be75e
AnikaC-git <anika.cawthorn@gmail.com>
2020-12-02 18:34:48 +0000

    adding correct URL for project git repo

--- packages/data_processing/setup.py
+++ packages/data_processing/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="Pubmed_data_processing", 
     version="0.0.1",
     author="",
     description="A small example package",
-    url="https://github.com/AnikaC-git/Protein_ranking_mining",  # todo: this will probably need to change
-    packages=setuptools.find_packages(),
+    url="https://github.com/AnikaC-git/Protein_score_mining",
+    packages=find_packages(),
     python_requires='>=3.8',
 )
