966928e6ec4983d3b865a01e4971a1fabb817c5e
AnikaC-git <anika.cawthorn@gmail.com>
2020-12-02 18:35:23 +0000

    shifting additional functions into utilities package

--- scripts/run_data_processing_XML.py
+++ scripts/run_data_processing_XML.py
@@ -6,38 +6,11 @@ from data/raw and write to data/processed. These settings can be updated accordi
 """
 
 import sys
-import yaml
-from pathlib import Path
 
 from data_processing.extract_publication_data import extract_publication_data_from_xml
-
-
-def read_config(_config: str):
-    """
-    Reads parameter settings required for operation from config file. Config file format is expected to be in yaml file
-    format.
-
-    :param _config: path to config file
-    :return: dictionary containing parameter settings for operation
-    ;rtype: dict
-    """
-    # todo: more elaborate error handling needed if file parser raises error
-    with open(_config) as file:
-        # loader returns dictionary of settings specified in config file
-        return yaml.load(file, Loader=yaml.FullLoader)
-
-
-def read_xml_file_names(_xml_folder: str):
-    """
-    Function to read names of XML files in specified folder. Convenience function to process multiple XML files at once.
-
-    :param _xml_folder: folder assumed to hold XML files with file names ending in .xml
-    :type: str
-    :return: a list of file names ending in ".xml" files in the specified folder
-    :rtype: list
-    """
-    return [x for x in Path(_xml_folder).iterdir() if x.is_file() and str(x).endswith(".xml")]
-
+from protein_score_utilities.convenience_functions_files import read_config
+from protein_score_utilities.convenience_functions_files import read_xml_file_names
+from protein_score_utilities.convenience_functions_database import create_database_connection
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -50,8 +23,8 @@ if __name__ == "__main__":
     config = read_config(sys.argv[1])
     fnames = read_xml_file_names(config['data_raw'])
 
-    # todo: read from config file
-    db_conn = ""
+    # todo: add handling for database reset depending on config; add message for when database is regenerated
+    db_conn = create_database_connection(config['data_processed'] + config['db_file'])
 
     for fname in fnames:
         print(fname)
