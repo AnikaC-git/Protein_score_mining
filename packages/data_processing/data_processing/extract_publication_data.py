a6ae297e131840d15c99b14a20e7b7a93caf10b1
AnikaC-git <anika.cawthorn@gmail.com>
2020-12-06 11:28:20 +0000

    tidying up docstrings

--- packages/data_processing/data_processing/extract_publication_data.py
+++ packages/data_processing/data_processing/extract_publication_data.py
@@ -1,31 +1,31 @@
 """
-Module that provides functionality for the extraction of text passages from Pubmed XML files. These XML
-files contain references for a large number of publications, where each publication may hold more or less
-specifying data. As a first attempt, the data that will be extracted for each publication, if present,
-are Pubmed ID, title, abstract, journal and publication year. The data will be held temporarily in
-SQLite database for easier querying.
+Module that provides functionality for the extraction of publication records. While different formats for publication
+sources will be supported in the future, the only functionality available at present is for the extraction of data from
+<a href="https://pubmed.ncbi.nlm.nih.gov/help/#download-pubmed-data">Pubmed XML files</a>.
 
-For now it is only XML files, but in the future, targeted support for PDF, docx or text files could be
-added here. Though this would very likely require additional functionality to determine the structure 
-of the document, e.g. through processing Core Scientific Concepts such as those described 
-<a href="http://www.lrec-conf.org/proceedings/lrec2016/pdf/676_Paper.pdf">here</a>.
+These XML files contain references for a large number of publications, where each publication may hold more or less
+specifying data. As a first attempt, the data that will be extracted for each publication, if present, are Pubmed ID,
+title, abstract, journal and publication year. The data will be held temporarily in SQLite database for easier querying.
 """
 
 
-def extract_publication_data_from_xml(file_path, db_conn):
+def extract_publication_data_from_xml(file_path: str, db_conn):
     """
     Extracts title, abstract, journal and so on for a publication contained in the Pubmed XML file.
     Provides summary information at the end about how many publications have been identified.
 
-    :param file_path:
-    :type:
-    :param db_conn:
-    :type:
+    :param file_path: path to XML file from which publication data is to be extracted
+    :type file_path: str
+    :param db_conn: connection to the database extracted publication information should be written to
+    :type db_conn: :class:~`sqlite3.Connection`
     """
     # todo: the approach for identifying relevant parts of the publication is very simplistic and 
     #  not very foolproof for now and should be changed going forward
     from lxml import etree
     context = etree.iterparse(file_path, events=('end',), tag="PubmedArticle")
+
+    # fields that are required for each extracted publication in order to be stored
+    # key is the internal reference and value the path of the element within the XML element
     elem_of_interest = {
         'PMID': 'MedlineCitation/PMID',
         'abstract': 'MedlineCitation/Article/Abstract/AbstractText',
@@ -34,23 +34,28 @@ def extract_publication_data_from_xml(file_path, db_conn):
         'pub_date_year': 'MedlineCitation/Article/Journal/JournalIssue/PubDate/Year'
     }
 
+    # add counters for summary stats
     counter_publications = 0
     counter_publications_incomplete = 0
 
+    # go through all the publications found in the XML file
     for event, elem in context:
         counter_publications += 1
         publication = {}
+
+        # attempt to extract all the relevant fields for each publication
         for key, value in elem_of_interest.items():
             eoi = elem.xpath(value)        
             # only take first entry found
+            # todo: this needs to be expanded, handled more carefully and details logged once logger is in place
             if len(eoi) > 0:
                 if eoi[0].text is not None:
                     publication[key] = eoi[0].text
             else:
                 break
-        
+
+        # only store publication in database if all the relevant data fields have been extracted
         if len(publication) == len(elem_of_interest):
-            print(publication['PMID'])
             save_publication_to_database(publication, db_conn)
         else:
             counter_publications_incomplete += 1
@@ -60,6 +65,7 @@ def extract_publication_data_from_xml(file_path, db_conn):
         while elem.getprevious() is not None:
             del elem.getparent()[0]
 
+    # print summary statistics
     print(f"Total number of publications found: {counter_publications}")
     print(f"Total number of incomplete publications found: {counter_publications_incomplete}")
                           
@@ -69,9 +75,9 @@ def save_publication_to_database(data: dict, db_conn):
     Save data extracted about a publication from one of the data sources to the publication database.
 
     :param data: publication data to be stored in database
-    :type: dict
-    :param db_conn: connector to database data should be stored in
-    :type: conn
+    :type data: dict
+    :param db_conn: connection to the database extracted publication information should be written to
+    :type db_conn: :class:~`sqlite3.Connection`
     :return: True if record stored successfully, otherwise False
     :rtype: bool
     """
