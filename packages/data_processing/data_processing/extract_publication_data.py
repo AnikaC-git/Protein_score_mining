50a05e0fbd0f8649753a547b3757c93c44ed1f14
AnikaC-git <anika.cawthorn@gmail.com>
2020-12-06 20:19:51 +0000

    Changing to data capsule for pbulication

--- packages/data_processing/data_processing/extract_publication_data.py
+++ packages/data_processing/data_processing/extract_publication_data.py
@@ -1,7 +1,7 @@
 """
 Module that provides functionality for the extraction of publication records. While different formats for publication
 sources will be supported in the future, the only functionality available at present is for the extraction of data from
-`Pubmed XML files <https://pubmed.ncbi.nlm.nih.gov/help/#download-pubmed-data>`_.
+Pubmed XML files <https://pubmed.ncbi.nlm.nih.gov/help/#download-pubmed-data>`_.
 
 These XML files contain references for a large number of publications, where each publication may hold more or less
 specifying data. As a first attempt, the data that will be extracted for each publication, if present, are Pubmed ID,
@@ -9,6 +9,41 @@ title, abstract, journal and publication year. The data will be held temporarily
 """
 
 from protein_score_utilities.convenience_functions_database import execute_insert_statement
+
+
+class Publication:
+    """
+    Data capsule for holding data relevant to a publication.
+    """
+    def __init__(self):
+        """
+        Initialising all the attributes with None.
+        """
+        self.pmid = None
+        self.abstract_text = None
+        self.title = None
+        self.journal = None
+        self.pub_year = None
+
+    def has_all_attributes(self):
+        return all(k is not None for k in (self.pmid, self.abstract_text, self.title, self.journal, self.pub_year))
+
+    def save_publication_to_database(self, db_conn):
+        """
+        Save data extracted about a publication from one of the data sources to the publication database.
+
+        :param data: publication data to be stored in database
+        :type data: dict
+        :param db_conn: connection to the database extracted publication information should be written to
+        :type db_conn: :class:~`sqlite3.Connection`
+        :return: True if record stored successfully, otherwise False
+        :rtype: bool
+        """
+
+        stm = "INSERT INTO publications(pmid,pub_abstract,title,journal,pub_year) VALUES(?,?,?,?,?)"
+
+        execute_insert_statement(db_conn, stm, (self.pmid, self.abstract_text, self.title, self.journal,
+                                                self.pub_year))
 
 
 def extract_publication_data_from_xml(file_path: str, db_conn):
@@ -31,11 +66,11 @@ def extract_publication_data_from_xml(file_path: str, db_conn):
     # fields that are required for each extracted publication in order to be stored
     # key is the internal reference and value the path of the element within the XML element
     elem_of_interest = {
-        'PMID': 'MedlineCitation/PMID',
-        'abstract': 'MedlineCitation/Article/Abstract/AbstractText',
+        'pmid': 'MedlineCitation/PMID',
+        'abstract_text': 'MedlineCitation/Article/Abstract/AbstractText',
         'title': 'MedlineCitation/Article/ArticleTitle',
         'journal': 'MedlineCitation/Article/Journal/Title',
-        'pub_date_year': 'MedlineCitation/Article/Journal/JournalIssue/PubDate/Year'
+        'pub_year': 'MedlineCitation/Article/Journal/JournalIssue/PubDate/Year'
     }
 
     # add counters for summary stats
@@ -45,7 +80,7 @@ def extract_publication_data_from_xml(file_path: str, db_conn):
     # go through all the publications found in the XML file
     for event, elem in context:
         counter_publications += 1
-        publication = {}
+        publication = Publication()
 
         # attempt to extract all the relevant fields for each publication
         for key, value in elem_of_interest.items():
@@ -54,15 +89,15 @@ def extract_publication_data_from_xml(file_path: str, db_conn):
             # todo: this needs to be expanded, handled more carefully and details logged once logger is in place
             if len(eoi) > 0:
                 if eoi[0].text is not None:
-                    publication[key] = eoi[0].text
+                    publication.__setattr__(key, eoi[0].text)
             else:
                 break
 
         # only store publication in database if all the relevant data fields have been extracted
-        if len(publication) == len(elem_of_interest):
+        if publication.has_all_attributes():
             # todo: there are a number of integrity errors raised through duplicated PMIDs that would need to be
             #  further investigated
-            save_publication_to_database(publication, db_conn)
+            publication.save_publication_to_database(db_conn)
         else:
             counter_publications_incomplete += 1
         
@@ -74,21 +109,4 @@ def extract_publication_data_from_xml(file_path: str, db_conn):
     # print summary statistics
     print(f"Total number of publications found: {counter_publications}")
     print(f"Total number of incomplete publications found: {counter_publications_incomplete}")
-                          
-                
-def save_publication_to_database(data: dict, db_conn):
-    """
-    Save data extracted about a publication from one of the data sources to the publication database.
 
-    :param data: publication data to be stored in database
-    :type data: dict
-    :param db_conn: connection to the database extracted publication information should be written to
-    :type db_conn: :class:~`sqlite3.Connection`
-    :return: True if record stored successfully, otherwise False
-    :rtype: bool
-    """
-
-    stm = "INSERT INTO publications(pmid,pub_abstract,title,journal,pub_year) VALUES(?,?,?,?,?)"
-
-    execute_insert_statement(db_conn, stm, (data['PMID'], data['abstract'], data['title'], data['journal'],
-                                            data['pub_date_year']))
